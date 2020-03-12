cb2a8e493d811a40612a892eaa0005221fa1a01b
FarHYP <1888213+FarHYP@users.noreply.github.com>
2020-03-12 11:30:49 +0800

    Add files via upload

---
+++ js/voile.js
@@ -0,0 +1,10 @@
+function voiler() {
+    document.getElementById("voile").style.display = "block";
+    document.getElementById("header").style.backgroundColor = "transparent";
+}
+
+function devoiler() {
+    document.getElementById("voile").style.display = "none";
+    document.getElementById("header").style.backgroundColor = "white";
+}
+ 
